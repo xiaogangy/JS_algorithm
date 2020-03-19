999665d7dade44e099951ca5c9134c61534725c3
xiaogangy <yangxiaogang2011@gmail.com>
2020-03-20 00:43:33 +0800

    链表

---
+++ 链表.js
@@ -0,0 +1,47 @@
+// Node节点
+function Node (data) {
+	this.data = data;
+	this.next = null;
+}
+
+// Linked-List
+// 操作应该放到原型链上
+// function LinkedList_simple() {
+// 	this.head = null;
+// 	// 插入
+// 	this.insert = function(index, node) {
+
+// 	};
+// 	// 查找
+// 	this.get = function(index) {
+
+// 	};
+// 	// 删除
+// 	this.delete = function(index) {
+
+// 	};
+// 	// 更改
+// 	this.modify = function(index, data) {
+
+// 	}
+// }
+
+function LinkedList() {
+	this.head = null;
+}
+// 插入
+LinkedList.prototype.insert = function(index, node) {
+
+};
+// 查找
+LinkedList.prototype.get = function(index) {
+
+};
+// 删除
+LinkedList.prototype.delete = function(index) {
+
+};
+// 更改
+LinkedList.prototype.modify = function(index, data) {
+
+}
