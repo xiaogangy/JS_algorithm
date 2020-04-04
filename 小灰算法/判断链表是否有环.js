745c0a45a25c1042f295be0dcea331d7eee321f6
xiaogangy <yangxiaogang2011@gmail.com>
2020-04-04 23:54:36 +0800

    链表有环

--- 小灰算法/判断链表是否有环.js
+++ 小灰算法/判断链表是否有环.js
@@ -2,6 +2,12 @@
  * @file 如何判断一个链表中是否有环？
  */
 
+// 链表节点
+function Node(data) {
+    this.data = data;
+    this.next = null;
+}
+
 // 解法1：暴力求解法
 // 每遍历到一个元素，就从链表的源头再开始进行另外一次测试遍历，查看这次测试遍历中是否有与当前访问节点相同的节点，如果有，那么就是有环
 // 这种方法就不实现了
@@ -12,7 +18,7 @@
 // 经典解法：追及问题
 // 考虑两个在操场跑步的人A和B，如果B的倍速是A的2倍，两个人从起点开始跑步，那么B肯定会再次追上A，第一次追上时差值最小，差值为一圈。也就是以两人会在跑道起点再次相遇
 // 借助这个思路，设置两个指针，一次走2步，一次走1步，如果二者会相遇，那么肯定是有环的
-function isCycle(head) {
+function hasCycle(head) {
     let first = head;
     let second = head;
     while (second !== null && second.next !== null) {
@@ -32,7 +38,7 @@ function isCycle(head) {
 // 参数如第一次相遇的节点，即继续开始走的起点
 function cycleLength(start) {
     // 记录A走的步数
-    const length = 0;
+    let length = 0;
     let first = start;
     let second = start;
     while (second !== null && second.next !== null) {
@@ -46,7 +52,6 @@ function cycleLength(start) {
 }
 
 
-
 /**
  * 引申2：如果有环，如何判断环的起点（入环起点）？
  * 思路：假设A和B在入环S1距离的地方相遇，从S1到入环点的距离为S2，从起点开始到入环点的距离为D，那么从上文可以得出以下公式
@@ -55,6 +60,47 @@ function cycleLength(start) {
  * 这里难理解的地方是，一开始我总认为n是一个变量，是一个不确定的值，但是其实n是一个定值，可以假定它为任何一个值，那么上述公式在A和B相遇时
  * 仍然是成立的，那么公式右边的意思是，绕了n圈（此时仍在原来的位置），然后继续前进了S2步，这就刚好是我们一开始设定的到达入环节点的距离；
  */
-function cycleIn() {
-    
+/**
+ * 
+ * @param {Object} 相遇的节点
+ * @param {Object} 头结点 
+ */
+function startPoint(node, head) {
+    // 相遇位置设置一个指针
+    let circleNode = node;
+    // 起点放置一个指针
+    let startNode = head;
+    // 只要二者没相遇，就让他们同时每次走一步
+    while (circleNode !== startNode) {
+        circleNode = circleNode.next;
+        startNode = startNode.next;
+    }
+    return circleNode;
 }
+
+
+function test() {
+    const node1 = new Node(5);
+    const node2 = new Node(3);
+    const node3 = new Node(7);
+    const node4 = new Node(2);
+    const node5 = new Node(6);
+    const node6 = new Node(8);
+    const node7 = new Node(1);
+    node1.next = node2;
+    node2.next = node3;
+    node3.next = node4;
+    node4.next = node5;
+    node5.next = node6;
+    node6.next = node7;
+    node7.next = node4;
+
+    // 判断是否有环
+    console.log('有环？', hasCycle(node1));
+    // 环的长度
+    console.log('环长？', cycleLength(node1));
+    // 环的入点
+    console.log('入点', startPoint(node1, node5));
+}
+
+test();
