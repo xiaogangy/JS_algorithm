55033d3ec65f0a0e6eb6912545c8572f44d3e704
yangxiaogang01 <yangxiaogang01@163.com>
2020-10-11 18:14:40 +0800

    2020-10-11复习

--- 剑指offer/抽象建模能力/圆圈中最后剩下的数字.js
+++ 剑指offer/抽象建模能力/圆圈中最后剩下的数字.js
@@ -1,6 +1,6 @@
 /**
  * @desc 0, 1, 2, ……, n-1这个n个数字排成一个圆圈，从数字0开始，每次从这个圆圈里删除第m个数字。求出这个圆圈里剩下的最后一个数字。其实就是约瑟夫环问题。举个例子，
- * 0, 1, 2, 3, 4这5个数字组成一个圆圈，从数字0开始每次删除第3个数字，则删除的前4个数字一次是2、0、4、1，最后剩下的数字是3。
+ * 0, 1, 2, 3, 4这5个数字组成一个圆圈，从数字0开始每次删除第3个数字，则删除的前4个数字依次是2、0、4、1，最后剩下的数字是3。
  *
  * solution1: 循环链表
  * 这种思路最简单，我们按照传入的n个数字，构造一个环形的链表，然后每次走m步，然后删除一个数字，然后继续进行。持续进行，直到最后剩下一个数字。这种解法比较直观，也最好理解，
@@ -11,6 +11,7 @@
  * 我们可以考虑将第一次删除后的数组往前拉m个元素，这样就又符合f(n, m)的定义了。因为不管公式怎样，最后得到的值肯定是一样的，所以可以得到f(n, m) = f(n-1, m) + m，但是又由于有可能数组越界，
  * 超过的部分会被接到头上，所以还要模 n，所以最后的递推公式为 f(n,m) = (f(n−1,m) + m) % n。具体的推导，可以查看剑指offer。
  * 递归的出口是什么呢？当n为1的时候，因为数组里只有一个数字，不需要进行删除，显然f(1, m) = 0;
+ * 推荐参考：https://blog.csdn.net/yanweibujian/article/details/50876631（关于约瑟夫环的一些思考）
  */
 
 function Node(value) {
@@ -31,7 +32,7 @@ function createCycle(n) {
     // 不要忘了最后把尾节点指向头结点
     pre.next = head;
 
-    return {last: pre, head};
+    return head;
 }
 
 function solution1(n, m) {
@@ -41,22 +42,24 @@ function solution1(n, m) {
     }
 
     // 1. 先构造环形链表
-    let {last: pre, head: cycle} = createCycle(n);
+    let cycle = createCycle(n);
 
     // 2. 循环删除，直到节点数为1
     // 因为删除节点的缘故，要声明一个指针指向待删除节点的前一个节点
-    // let pre = new Node();
-    // pre.next = cycle;
-    // @date 8.11更改：上面是原先的写法，虽然程序最后输出没错，但是在每次删除环中第一个节点时，环的结构不太对，所以改成将pre设置为环中的最后一个节点
+    let pre = null;
     let toBeDel = cycle;
 
     let i = n;
     while (i > 1) {
-        // 找到第m个节点，然后删除
-        for (let j = 1; j < m; j++) {
-            pre = pre.next;
+        // 删除第m个节点，所以要走m-1步；但是又为了拿到第m个节点的前一个节点，所以循环里再少走一步
+        let j = 0;
+        while (j + 1 < m - 1) {
             toBeDel = toBeDel.next;
+            j++;
         }
+        pre = toBeDel;
+        toBeDel = toBeDel.next;
+
         // 删除节点
         pre.next = toBeDel.next;
 
